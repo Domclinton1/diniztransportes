f71b05b55cbb63940878573f2458540b22c9f74f
Clinton <clinton.admpro29@gmail.com>
2025-06-16 16:31:41 -0300

    ...

---
+++ script.js
@@ -0,0 +1,9 @@
+// FAQ toggle logic
+const questions = document.querySelectorAll('.faq-question');
+
+questions.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const answer = btn.nextElementSibling;
+    answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+  });
+});
